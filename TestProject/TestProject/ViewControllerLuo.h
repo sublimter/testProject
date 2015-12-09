8c2578f68d854621ed565851a8dc2497d822b830
sublimter <1396037340@qq.com>
2015-12-09 14:54:25 +0800

    2015-12-9 2:54 luo的测试功能模块ok

---
+++ TestProject/TestProject/ViewControllerLuo.h
@@ -0,0 +1,15 @@
+//
+//  ViewControllerLuo.h
+//  TestProject
+//
+//  Created by luo on 15/12/9.
+//  Copyright © 2015年 codeBaby. All rights reserved.
+//
+/**
+ *  我的测试功能
+ */
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerLuo : UIViewController
+
+@end
---
+++ TestProject/TestProject/ViewControllerLuo.m
@@ -0,0 +1,24 @@
+//
+//  ViewControllerLuo.m
+//  TestProject
+//
+//  Created by luo on 15/12/9.
+//  Copyright © 2015年 codeBaby. All rights reserved.
+//
+
+#import "ViewControllerLuo.h"
+
+@interface ViewControllerLuo ()
+
+@end
+
+@implementation ViewControllerLuo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+
+@end
